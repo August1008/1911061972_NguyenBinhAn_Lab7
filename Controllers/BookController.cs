b1d2613092fba70e0077e19ea211a2959942c51b
ngbin <ngbin@DESKTOP-GB9K06E>
2021-07-21 13:29:43 +0700

    Nộp bài 7 . Hoàn thành

---
+++ Controllers/BookController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using _1911061972_NguyenBinhAn_Lab7.Models;
+namespace _1911061972_NguyenBinhAn_Lab7.Controllers
+{
+    public class BookController : ApiController
+    {
+        public List<Book> books = new List<Book>
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "The Lord Of The Ring",
+                Content = "The Lord Of The Ring",
+                Author = "J.R.R Tolkien",
+                Price = 99
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "The Secret Of The Sword",
+                Content = "The Secret Of the Sword",
+                Author = "J.R.R Tolkien",
+                Price = 69
+            }
+        };
+
+        public List<Book> GetBooks()
+        {
+            return books;
+        }
+
+        public IHttpActionResult GetBookById(int Id)
+        {
+            var book = books.FirstOrDefault(b =>b.Id == Id);
+            if (book == null) return NotFound();
+            return Ok(book);
+        }
+    }
+}
+    
+
---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1911061972_NguyenBinhAn_Lab7.Models
+{
+    public class Book
+    {
+        public int Id { set; get; }
+        public string Title { set; get; }
+        public string Content { set; get; }
+        public string Author { set; get; }
+        public double Price { set; get; }
+    }
+}
